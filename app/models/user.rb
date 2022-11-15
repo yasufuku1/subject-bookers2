c8943be6f84e07ac1f50d6c1c88a608396ed7255
EC2 Default User <ec2-user@ip-172-31-4-148.ap-northeast-1.compute.internal>
2022-11-15 09:04:25 +0000

    [Update]db migrationまで

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+
+  has_one_attached :profile_image
+
+  validates :name,
+    length: { minimum: 2, maximum: 20 }
+
+  validates :introduction,
+    length: {maximum: 50 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
+  resources :books, only:[:new,:show,:index]
+  #get 'users/show'
+  #get 'users/edit'
+  #get 'books/new'
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/edit'
+  #get 'homes/top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "homes#top"
 end
